90993e9fca036655ca0f7f3bccc41b5383b62cf6
edson carlos <edsoncarlos.dev@gmail.com>
2024-04-06 06:40:49 -0300

    add Goal based agent

---
+++ src/GoalBasedAgent/GoalBasedAgent.test.ts
@@ -0,0 +1,50 @@
+import { RealEnvironment } from "../Environment";
+import { Perception } from "../Perception";
+import { GoalBasedAgent } from "./GoalBasedAgent";
+import { AgentActuor } from "../AgentActuor";
+
+const environment = new RealEnvironment();
+const perception = new Perception(environment);
+const agent = new GoalBasedAgent(new AgentActuor(environment, perception), environment.getItens());
+
+
+it ('start environment', () => {
+    const itens = environment.getItens();
+
+    expect(itens).toHaveLength(10);
+    expect(itens).not.toBeNull();
+})
+
+describe('', () => {
+    
+    
+    it ('End environment', () => {
+        let isWork = true;
+        while(isWork) {
+            perception.updatePerception();
+            const action = agent.agentFunction(perception);
+            
+            isWork = agent.actuor(action);
+        }    
+
+        const expectedEnvironment = new Array(10);
+        for (let i = 0; i < 10; i++) {
+            expectedEnvironment[i] = new Array(10).fill(0);    
+        }
+        expectedEnvironment[0][0] = 1;
+
+        const itens = environment.getItens();
+        const pos = [];
+        for (let i = 0; i < itens.length; i++) {
+            pos.push(itens[i].posx);
+            pos.push(itens[i].posy);
+        }
+        
+        expect(pos).toContain(0);
+        expect(pos).not.toContain(10);
+        expect(pos).not.toContain(20);
+        expect(expectedEnvironment).toEqual(environment.environment);
+    })
+})
+
+
---
+++ src/GoalBasedAgent/GoalBasedAgent.ts
@@ -0,0 +1,98 @@
+import { Actions } from "../Actions";
+import { AgentActuor } from "../AgentActuor";
+import { Perception } from "../Perception";
+import { Environment, Item } from "../Environment";
+import { AgentI } from "../AgentI";
+
+export class GoalBasedAgent implements AgentI {
+    private agentActuor;
+    private isEmpty = true;
+    
+    //world
+    private environment: Array<Array<number>>; 
+    private itens: Array<Item>;
+
+    private goalx;
+    private goaly;
+    
+    constructor(actuor: AgentActuor, itens: Array<Item>) {
+        this.agentActuor = actuor;
+        this.environment = new Array(Environment.n);
+
+        for (let i = 0; i < Environment.n; i++) {
+            this.environment[i] = new Array(Environment.m).fill(0);    
+        }
+        this.itens = [...itens];
+        [this.goalx, this.goaly] = this.getPosItem();
+    }
+
+    private getPosItem(): Array<number> {
+        if (this.itens.length > 0) {
+            const x = this.itens[0].posx;
+            const y = this.itens[0].posy;
+    
+            if (x == 0 && y == 0) {
+                this.itens.shift();
+                return this.getPosItem();
+            }
+            return [x, y];
+        }
+        return [-1, -1];
+    }
+
+    public agentFunction(perception: Perception): Actions {
+        const {
+            startPosx,
+            startPosy,
+            posx,
+            posy,
+            content } = perception;
+        
+        this.environment[posx][posy] = 1;
+
+        //drop
+        if (posx == startPosx && posy == startPosy && !this.isEmpty) {
+            this.isEmpty = !this.isEmpty;
+            [this.goalx, this.goaly] = this.getPosItem();
+
+            return Actions.drop;
+        }
+
+        // move item to start point
+        if (!this.isEmpty) {
+            if (posy > 0) {
+                return Actions.up;
+            }
+            if (posx > 0) {
+                return Actions.left;
+            }
+        }
+
+        if (posx != startPosx || posy != startPosy) {
+            if (content != null) {
+                // get item
+                if (this.isEmpty)  {
+                    this.isEmpty = !this.isEmpty;
+                    return Actions.get;
+                }
+            }
+        }
+
+        // move to goal position
+        if (this.goalx != -1 && this.goaly != -1) {
+            if (posx < this.goalx) {
+                return Actions.right;
+            }
+
+            if (posy < this.goaly) {
+                return Actions.down
+            }
+        }
+
+        return Actions.NoOp;
+    }
+
+    public actuor(action: Actions) {
+        return this.agentActuor.actuors(action);
+    }
+}
